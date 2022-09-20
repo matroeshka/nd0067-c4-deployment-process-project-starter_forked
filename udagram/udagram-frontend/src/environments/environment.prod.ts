f0dc0c38323532956c5c3f03054bb7f18c713e55
Jennifer Sergeev <js@mail.com>
2022-09-20 19:29:19 +0200

    remove changes

--- udagram/udagram-frontend/src/environments/environment.prod.ts
+++ udagram/udagram-frontend/src/environments/environment.prod.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: true,
   appName: 'Udagram',
-  apiHost: 'http://udagram-api-dev.eba-6k9isnpz.us-east-1.elasticbeanstalk.com/api/v0/',
+  apiHost: 'http://udagram-api-dev.eba-6k9isnpz.us-east-1.elasticbeanstalk.com/api/v0',
 };
 
 /*
