8cd473dfd14067d3ca13a50d7fc7311a57a9610c
Jennifer Sergeev <js@mail.com>
2022-09-20 07:17:04 +0200

    hosting

--- udagram/udagram-api/src/controllers/v0/feed/routes/feed.router.ts
+++ udagram/udagram-api/src/controllers/v0/feed/routes/feed.router.ts
@@ -69,7 +69,6 @@ router.post('/',
         return res.status(400).send({message: 'File url is required.'});
       }
       
-      //@ts-ignore
       const item = await new FeedItem({
         caption: caption,
         url: fileName,
--- udagram/udagram-api/src/server.ts
+++ udagram/udagram-api/src/server.ts
@@ -10,13 +10,6 @@ import { V0_FEED_MODELS, V0_USER_MODELS } from "./controllers/v0/model.index";
 
 (async () => {
   dotenv.config();
-  
-  try {
-    await sequelize.authenticate();
-    console.log("Connection has been established successfully.");
-  } catch (error) {
-    console.error("Unable to connect to the database:", error);
-  }
 
   await sequelize.addModels(V0_FEED_MODELS);
   await sequelize.addModels(V0_USER_MODELS);
@@ -25,25 +18,26 @@ import { V0_FEED_MODELS, V0_USER_MODELS } from "./controllers/v0/model.index";
   console.log("Database Connected");
 
   const app = express();
-  const port = 8080;
+  const port = process.env.PORT || 8080;
 
   app.use(bodyParser.json());
 
-  // app.use(cors());
+   app.use(cors());
   // We set the CORS origin to * so that we don't need to
   // worry about the complexities of CORS. 
-  app.use(cors({
-    "allowedHeaders": [
-      'Origin', 'X-Requested-With',
-      'Content-Type', 'Accept',
-      'X-Access-Token', 'Authorization', 'Access-Control-Allow-Origin',
-      'Access-Control-Allow-Headers',
-      'Access-Control-Allow-Methods'
-    ],
-    "methods": 'GET,HEAD,OPTIONS,PUT,PATCH,POST,DELETE',
-    "preflightContinue": true,
-    "origin": '*',
-  }));
+  // app.use(cors({
+  //   "allowedHeaders": [
+  //     'Origin', 'X-Requested-With',
+  //     'Content-Type', 'Accept',
+  //     'X-Access-Token', 'Authorization',
+  //     'Access-Control-Allow-Origin',
+  //     'Access-Control-Allow-Headers',
+  //     'Access-Control-Allow-Methods'
+  //   ],
+  //   "methods": 'GET,HEAD,OPTIONS,PUT,PATCH,POST,DELETE',
+  //   "preflightContinue": true,
+  //   "origin": '*',
+  // }));
 
   app.use("/api/v0/", IndexRouter);
 
